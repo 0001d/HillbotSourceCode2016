5db5657e085ff787d1ad64815f9ffdb5ac2c4303
0001D LLC <0001des@gmail.com>
2016-09-16 09:53:36 -0700

    Added first draft
    Added first draft

---
+++ HillBotSourceCode2016/HillBotSourceCode2016/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace HillBotSourceCode2016
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(625, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ask A Hillbot:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(20, 54);
+            this.labelOutput.MaximumSize = new System.Drawing.Size(550, 0);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(10, 13);
+            this.labelOutput.TabIndex = 2;
+            this.labelOutput.Text = ".";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::HillBotSourceCode2016.Properties.Resources.hillary;
+            this.pictureBox1.Location = new System.Drawing.Point(586, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 181);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Hillbot Programming ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HillBotSourceCode2016/HillBotSourceCode2016/Form1.cs
@@ -0,0 +1,142 @@
+﻿/* ///////////////////////////////////////////////////////////////////////////////////////////////////////
+ * Title:       Hillbot Programming
+ * 
+ * Desc:        The actual source code for Hillbots (Hillary Supporters)
+ * Version:     2016 Campaign (upgraded from version 2008)
+ * Owners:      George Soros, Saudi Royal Family, Wall Street, various Clinton Foundation contributors.
+ * Reference:   https://www.hillaryclinton.com/ http://www.cnn.com/ MSM, etc.
+ * Copyright:   2016 (c)
+ * Author:      Yeah right. I like breathing.
+ * Special thanks to contributors:  Henry Kissinger, Barack Obama, and FBI for nixing the indictment.
+ * 
+ * */
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HillBotSourceCode2016
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            bool control = (2 + 2 == 5);
+
+            string HilQuestion = textBox1.Text.ToString();
+            if (HilQuestion.IndexOf("clinton foundation", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "You need to do more research.  The Clinton Foundation is a great organization, " +
+                    "and they only do good work.  To say otherwise is extreme sexism.";
+            }
+            else if ((HilQuestion.IndexOf("cnn", StringComparison.CurrentCultureIgnoreCase) >= 0) || 
+                (HilQuestion.IndexOf("msnbc", StringComparison.CurrentCultureIgnoreCase) >= 0) ||
+                (HilQuestion.IndexOf("media", StringComparison.CurrentCultureIgnoreCase) >= 0) ||
+                (HilQuestion.IndexOf("msm", StringComparison.CurrentCultureIgnoreCase) >= 0))
+            {
+                labelOutput.Text = "ACTUALLY, the media has been very critical of Hillary and it's mean. " +
+                    "By the way, you are sexist.";
+            }
+            else if ((HilQuestion.IndexOf("bernie", StringComparison.CurrentCultureIgnoreCase) >= 0) ||
+                (HilQuestion.IndexOf("sanders", StringComparison.CurrentCultureIgnoreCase) >= 0))
+            {
+                labelOutput.Text = "Berniebros.  So sick of Berniebros being mean with all their facts." +
+                    "They are sooooooooooo sexist.";
+            }
+            else if (HilQuestion.IndexOf("trump", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "Misogynist.  Just stop.  Deplorables.  Can't even.  I get allergic." +
+                    "";
+            }
+            else if (HilQuestion.IndexOf("debbie", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "Great woman and a DNC hero.  She didn't rig anything.  You are so sexist." +
+                    "";
+            }
+            else if ((HilQuestion.IndexOf("sick", StringComparison.CurrentCultureIgnoreCase) >= 0) || 
+                (HilQuestion.IndexOf("health", StringComparison.CurrentCultureIgnoreCase) >= 0) )
+            {
+                labelOutput.Text = "HILLARY CLINTON IS NOT SICK.  YOU ARE SICK IN THE MIND AND" +
+                    "ONE THE DEPLORABLE ANTI-CLINTON SEXISTS WHO CANNOT HANDLE A STRONG WOMAN.";
+            }
+            else if (HilQuestion.IndexOf("health care", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "Hillary was an innovator back when she wanted healthcare in the 90's.  " + 
+                    "The fact that she reneged once she was bought by the health industry, is of no consequence.";
+            }
+            else if (HilQuestion.IndexOf("am i sexist", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "Yes." +
+                    "";
+            }
+            else if (HilQuestion.IndexOf("dnc", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "Look.  The DNC is extremely fair and impartial.  They didn't rig the primary " +
+                    "and didn't collude with the media to hide Clinton's faults.  To allude otherwise would be sexist.";
+            }
+            else if (HilQuestion.IndexOf("wall", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "Who cares about those speeches??? She isn't beholden to those bankers and speaks for us. " +
+                    "Do you really want a Trump presidency!!!!???";
+            }
+            else if (HilQuestion.IndexOf("corporat", StringComparison.CurrentCultureIgnoreCase) >= 0)
+            {
+                labelOutput.Text = "We must do everything to defeat Trump!" +
+                    "Do you really want a Trump presidency!!!!???";
+            }
+
+
+
+            else
+            {
+                // If someone criticizes Hillary, and you don't understand it, 
+                // you dislike their point, 
+                // or can't think of anything, 
+                // ALWAYS DEFAULT TO SEXISM ACCUSATION!
+                if ((HilQuestion.Length > 5) && (HilQuestion.Length < 10))
+                {
+                    labelOutput.Text = "You are sexist.";
+                }
+                else if ((HilQuestion.Length >= 10) && (HilQuestion.Length < 15))
+                {
+                    labelOutput.Text = "OMG Shut up.  You are so sexist.";
+                }
+                else if ((HilQuestion.Length >= 15) && (HilQuestion.Length < 20))
+                {
+                    labelOutput.Text = "Does Trump have you on his payroll???  You are SEXIST.";
+                }
+                else if ((HilQuestion.Length >= 20) && (HilQuestion.Length < 25))
+                {
+                    labelOutput.Text = "You are SO DEPLORABLE.  You are SEXIST, RACIST, XENOPHOBIC, HOMOPHOBIC...";
+                }
+                else if (HilQuestion.Length >= 25)
+                {
+                    labelOutput.Text = "SHE WILL BE THE FIRST WOMAN PRESIDENT REGARDLESS OF YOUR EXTREME SEXISM, " +
+                        "RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, SEXISM, RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, " + 
+                        "RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, SEXISM, RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, " +
+                        "RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, SEXISM, RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, " +
+                        "RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, SEXISM, RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, " +
+                        "RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, SEXISM, RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, " +
+                        "RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC, SEXISM, RACISM, XENOPHOBIA, TRANSPHOBIC, HOMOPHOBIC... " ;
+                }
+
+            }
+
+
+        }
+    }
+}
